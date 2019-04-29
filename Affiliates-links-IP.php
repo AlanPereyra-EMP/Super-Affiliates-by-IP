81a802d0780e065aa917ebea43bb13eff53aac20
alanpereyra57 <alan@pereyra.online>
2019-04-29 20:23:13 -0300

    Super affiliates by Informatica Pereyra

--- Affiliates-links-IP.php
+++ Affiliates-links-IP.php
@@ -1,6 +1,6 @@
 <?php 
 /**
-* Plugin Name: Affiliates link By Informatica Pereyra
+* Plugin Name: Super Affiliates By Informatica Pereyra
 * Plugin URI: https://informatica.pereyra.online/
 * Description: WordPress plugin to create differents affiliates links.
 * Version: 0.0.1 alpha
@@ -11,20 +11,20 @@
 // Detect affiliates link. 
   
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-    $alip_link = "https"; 
+    $saip_affiliate_link = "https"; 
 }
 else {
-    $alip_link = "http"; 
+    $saip_link = "http"; 
 }
   
 // Here append the common URL characters. 
-$alip_link .= "://"; 
+$saip_link .= "://"; 
   
 // Append the host(domain name, ip) to the URL. 
-$alip_link .= $_SERVER['HTTP_HOST']; 
+$saip_link .= $_SERVER['HTTP_HOST']; 
   
 // Append the requested resource location to the URL 
-$alip_link .= $_SERVER['REQUEST_URI']; 
+$saip_link .= $_SERVER['REQUEST_URI']; 
       
 // Print the link 
 	
